337e71e90a3c1f8b457b2e7b69f8378d18f1bca7
Doğan <dogantatarjr@gmail.com>
2025-08-15 15:39:55 +0300

    spatie-laravel-permissions and writer role setup

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $writerRole = Role::create(['name' => 'writer']); // Yazar Rolü
+        $editArticles_Permission = Permission::create(['name' => 'edit articles']); // Makaleleri Düzenleme İzni
+
+        $writerRole->givePermissionTo($editArticles_Permission); // Yazar rolüne makaleleri düzenleme izni verilir.
+
+        $user = User::where('email', 'writer1@example.com')->first();
+        $user->assignRole($writerRole); // Kullanıcıya yazar rolü atanır.
+    }
+}
