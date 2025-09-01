a1be2d43427a5007da302eb7a923b9518ab8b37c
Doğan <dogantatarjr@gmail.com>
2025-09-01 15:06:02 +0300

    user seeder created

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Doğan TATAR',
+            'email' => 'dogantatarjr@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Aydan COMBA',
+            'email' => 'aydancemrecomba@gmail.com',
+            'password'=> bcrypt('12345678'),
+            'role' => 'writer',
+        ]);
+
+        User::create([
+            'name' => 'Kuzey GÖKTEPE',
+            'email' => 'kuzey.goktepe@gmail.com',
+            'password'=> bcrypt('12345678'),
+            'role' => 'user',
+        ]);
+    }
+}
